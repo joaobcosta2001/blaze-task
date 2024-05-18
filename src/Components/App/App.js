a9f11d18370a8283cf7151da584f918357885c3c
joaobcosta2001 <joaoassuncaoecosta@gmail.com>
2024-05-18 22:59:03 +0100

    added google analytics

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -2,14 +2,23 @@ import './App.css';
 import Homepage from "../Homepage/Homepage"
 import Services from "../Services/Services"
 import Navbar from "../Navbar/Navbar"
-import { BrowserRouter,Route,Routes } from 'react-router-dom';
+import { BrowserRouter,Route,Routes, useLocation } from 'react-router-dom';
 import "../i18n/i18n";
 import Footer from '../Footer/Footer';
 import Contacts from '../Contacts/Contacts';
 import AboutUs from '../AboutUs/AboutUs'
 import NotFound from '../NotFound/NotFound';
+import ReactGA from 'react-ga';
 
 function App() {
+
+  ReactGA.initialize('G-511H5GLMN3');
+  const location = useLocation();
+  useEffect(() => {
+    ReactGA.pageview(window.location.pathname + window.location.search);
+  }, [location]);
+
+
   return (
     <BrowserRouter>
     
